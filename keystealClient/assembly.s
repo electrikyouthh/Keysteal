cb16d9ad42b6765409b17ce83f32bbd42b8af89a
Electrik Youth <electrikyouthx@hotmail.com>
2022-11-27 07:03:06 -0800

    .

--- keystealClient/assembly.s
+++ keystealClient/assembly.s
@@ -5,3 +5,11 @@
 //  Created by Mila Masaya on 11/27/22.
 //  Copyright © 2022 Mila Masaya. All rights reserved.
 //
+
+.intel_syntax noprefix
+
+.text
+
+#define DEFINE_MACH_TRAP(name, number) .globl name; name: mov rax, 0x1000000+number; mov r10, rcx; syscall; ret
+
+DEFINE_MACH_TRAP(_my_mach_msg_trap, 31)
